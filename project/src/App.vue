ef06124f1465e47d7e9aa08ecb6b4396ba035aef
Zhc <995662315@qq.com>
2019-01-03 21:29:56 +0800

    项目雏形

---
+++ project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <p>已选图表：</p>
+
+   
+    <ul>
+      <li v-on:click='buju(1)'>布局1</li>
+      <li v-on:click='buju(2)'>布局2</li>
+      <li v-on:click='buju(3)'>布局3</li>
+      <li v-on:click='buju(4)'>布局4</li>
+    </ul>
+     <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods:{
+    buju:function(num){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+
+
+}
+p{
+  color: #FFF;
+}
+li{
+  color: #FFF;
+}
+</style>
---
+++ project/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <syou-pie-chart
+          class="pie-chart"
+        />
+
+       <syou-gauge-chart 
+        id='index-1'
+       right='550' 
+        container="gauge1" 
+        class="gauge-chart" 
+        value='100'
+        name='签约评估得分'
+      />
+      <syou-gauge-chart
+        id='index-2'
+        right='800'
+        container="gauge2" 
+        class="gauge-chart"
+        value='90'
+        name='达标评估得分'
+      />
+  </div>
+</template>
+
+<script>
+
+//import SyouMapboxView from './SyouMapboxView.vue'
+import SyouPieChart from './SyouPieChart.vue'
+//import SyouRadarChart from './SyouRadarChart'
+//import SyouBubbleChart from './SyouBubbleChart.vue'
+import SyouGaugeChart from './SyouGaugeChart.vue'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  components:{
+    //SyouMapboxView
+    SyouPieChart,
+    //SyouRadarChart
+   //SyouBubbleChart
+   SyouGaugeChart
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
